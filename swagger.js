f264abb95dd17c1687110dcfaab2757f1dc058f6
Daniel C. Opute <danielcopute@gmail.com>
2024-12-07 21:06:27 +0000

    added api render link and changed github callback to use render link

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'BookSphere Api'
     },
     host: 'localhost:5000',
-    schemes: ['http', 'https']
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
