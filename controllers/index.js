bc90609e9fcb4536551a4649a431b70f2ef3ab6b
Daniel C. Opute <danielcopute@gmail.com>
2024-12-09 11:33:19 +0100

    correct the name booksphere in home menu from "bookshere" to "booksphere"

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,10 @@
 async function displayHome(req, res) {
     //#swagger.tags=['Home']
     const homeData = {
-        welcomeMsg: `Welcome to Team Thursday 1pm MT Web Services BookShere! BookSphere API is a tool that makes managing and accessing library resources easy and efficient.`,
+        welcomeMsg: `Welcome to Team Thursday 1pm MT Web Services BookSphere! BookSphere API is a tool that makes managing and accessing library resources easy and efficient.`,
         apiDocs: `${process.env.SERVER_HOST}/${process.env.SERVER_PORT}/api-docs`,
-        loginState: req.session.user !== undefined ? `Your are loggedIn as ${req.session.user.displayName};`: 'Logged Out'
+        loginState: req.session.user !== undefined ? `Your are loggedIn as ${req.session.user.displayName};`: 'Logged Out',
+        userRight: req.session.user != undefined ? `User Right: ${req.session.user.accountType}` : 'external-user'
     }
     res.send(homeData);
 }
